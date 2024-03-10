e35a63c2243103da8f9aab99a62c3d3cda41418e
dingrui <Bannirui@outlook.com>
2024-03-10 18:18:15 +0800

    cpp编译过程

---
+++ cpp/02/src/log.cpp
@@ -0,0 +1,9 @@
+// 预处理 copy paste
+#include <iostream>
+
+// 定义函数
+void Log(const char *msg)
+{
+    // 操作符<<
+    std::cout << msg << std::endl;
+}
---
+++ cpp/02/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#define INTEGER int
+
+// 函数声明
+void Log(const char *msg);
+
+INTEGER main()
+{
+    Log("Hello World");
+    std::cin.get();
+    return 0;
+#include "endBrace.h"
