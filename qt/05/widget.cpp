9e4d77b17c60bd81217a4cbcf327219bc2a44cf1
dingrui <Bannirui@outlook.com>
2024-06-30 19:36:03 +0800

    滚动条

--- qt/05/widget.cpp
+++ qt/05/widget.cpp
@@ -7,12 +7,39 @@
 #include "widget.h"
 #include "ui_Widget.h"
 
-
-Widget::Widget(QWidget *parent) :
-    QWidget(parent), ui(new Ui::Widget) {
-    ui->setupUi(this);
+Widget::Widget(QWidget* parent) :
+	QWidget(parent), ui(new Ui::Widget)
+{
+	ui->setupUi(this);
+	connect(this->ui->dial, &QSlider::valueChanged, this, &Widget::do_value_changed);
+	connect(this->ui->horizontalSlider, &QSlider::valueChanged, this, &Widget::do_value_changed);
+	connect(this->ui->horizontalScrollBar, &QSlider::valueChanged, this, &Widget::do_value_changed);
 }
 
-Widget::~Widget() {
-    delete ui;
+Widget::~Widget()
+{
+	delete ui;
+}
+void Widget::do_value_changed(int v)
+{
+	this->ui->dial->setValue(v);
+	this->ui->horizontalSlider->setValue(v);
+	this->ui->horizontalScrollBar->setValue(v);
+	this->ui->progressBar->setValue(v);
+}
+void Widget::on_checkBox_clicked(bool checked)
+{
+	this->ui->progressBar->setTextVisible(checked);
+}
+void Widget::on_checkBox_2_clicked(bool checked)
+{
+	this->ui->progressBar->setInvertedAppearance(checked);
+}
+void Widget::on_radioButton_clicked()
+{
+	this->ui->progressBar->setFormat("%p%");
+}
+void Widget::on_radioButton_2_clicked()
+{
+	this->ui->progressBar->setFormat("%v%");
 }
--- qt/05/widget.h
+++ qt/05/widget.h
@@ -7,21 +7,30 @@
 
 #include <QWidget>
 
-
 QT_BEGIN_NAMESPACE
-namespace Ui { class Widget; }
+namespace Ui
+{
+	class Widget;
+}
 QT_END_NAMESPACE
 
-class Widget : public QWidget {
-Q_OBJECT
+class Widget : public QWidget
+{
+	Q_OBJECT
 
-public:
-    explicit Widget(QWidget *parent = nullptr);
-    ~Widget() override;
+ public:
+	explicit Widget(QWidget* parent = nullptr);
+	~Widget() override;
 
-private:
-    Ui::Widget *ui;
+ private:
+	Ui::Widget* ui;
+
+ private slots:
+	void do_value_changed(int v);
+	void on_checkBox_clicked(bool checked);
+	void on_checkBox_2_clicked(bool checked);
+	void on_radioButton_clicked();
+	void on_radioButton_2_clicked();
 };
-
 
 #endif //INC_05__WIDGET_H_
