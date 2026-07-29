d377fb4cd2cb913bc33a5bdc073c59f9d080cab4
dingrui <Bannirui@outlook.com>
2026-07-29 22:45:16 +0800

    gradio应用

--- gradio/demo/main.py
+++ gradio/demo/main.py
@@ -1,4 +1,18 @@
+import gradio as gr
 
+def greet(name:str,intensity:float)->str:
+    r"""
+    功能函数
+    """
+    return "Hello, "+name+"!"*int(intensity)
+
+# 界面配置
+app=gr.Interface(
+        fn=greet,
+        inputs=["text","slider"],
+        outputs=["text"],
+        api_name="predict"
+    )
 
 if __name__ == '__main__':
-    print("hello world")
+    app.launch()
